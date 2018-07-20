8548ef3b164ad370ede422231add37408c8d6a1b
Chimereze Ikenna-Alfred <alfred.chimereze@gmail.com>
2018-07-20 13:33:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var name = "Joe"
 var height = "74"
 
-if (name === susan)
+if (name === susan) {
+  console.log("name is correct")
+} else {
+  console.log("expected the name to be susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
